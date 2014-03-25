2c9bb61cce2d9c025f92a5525b6936ff7d58f56f
Eser Utku <eser_utku@yahoo.com>
2014-03-25 22:35:59 +0000

    Carrying on with testing

---
+++ M2mProject/M2mDao/src/test/java/uk/co/kayratech/m2m/platform/dao/integration/support/LovTypeTxSupport.java
@@ -0,0 +1,7 @@
+package uk.co.kayratech.m2m.platform.dao.integration.support;
+
+import uk.co.kayratech.m2m.platform.model.lov.LovType;
+
+public interface LovTypeTxSupport extends BaseTxSupport<LovType> {
+
+}
---
+++ M2mProject/M2mModel/src/main/java/uk/co/kayratech/m2m/platform/model/lov/UserType.java
@@ -0,0 +1,12 @@
+package uk.co.kayratech.m2m.platform.model.lov;
+
+import javax.persistence.Entity;
+
+@Entity
+public class UserType extends LovValue {
+
+	private static final long serialVersionUID = 6589823147098421116L;
+	
+	public UserType() {
+	}
+}
